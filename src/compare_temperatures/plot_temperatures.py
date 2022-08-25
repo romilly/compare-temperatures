f11a80b29455591e0fb5d99a6ae260903cb7bc0b
Romilly Cocking <romilly.cocking@gmail.com>
2022-08-25 06:30:59 +0100

    Now plotting time series of temperatures, difference.

--- src/compare_temperatures/plot_temperatures.py
+++ src/compare_temperatures/plot_temperatures.py
@@ -8,11 +8,12 @@ def read_adafruit_feed_csv(csv, datacol, yesterday):
     df = pandas.read_csv(csv, parse_dates=['created_at'])
     df.dropna(axis=1, how='all',inplace=True)
     df = df[df['created_at']>= yesterday]
-    df = df.drop(['id','feed_id'],axis='columns')
+    df = df.drop(columns=['id','feed_id'])
     df.rename(columns={'value' : datacol}, inplace=True)
     df['created'] = df['created_at'].map(lambda x: x.replace(second=0))
-    df = df.drop('created_at', axis='columns')
-    df.reindex(df['created'])
+    df = df.drop(columns=['created_at'])
+    df = df.set_index(df['created'])
+    df = df.drop(columns=['created'])
     return df
 
 
